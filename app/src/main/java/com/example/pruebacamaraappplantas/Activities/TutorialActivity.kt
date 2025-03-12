b850a07265573d83113bd12a3d72eed79774dd52
davpalmol <davpalmol@alu.edu.gva.es>
2025-03-12 13:01:41 +0100

    2.2 Tutorial provisional finalitzat, proseguim en el home fragment

--- app/src/main/java/com/example/pruebacamaraappplantas/Activities/TutorialActivity.kt
+++ app/src/main/java/com/example/pruebacamaraappplantas/Activities/TutorialActivity.kt
@@ -1,26 +1,148 @@
 package com.example.pruebacamaraappplantas.Activities
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.pruebacamaraappplantas.Adapters.TutorialNavigationListener
-import com.example.pruebacamaraappplantas.Adapters.TutorialPagerAdapter
 import com.example.pruebacamaraappplantas.R
-import com.example.pruebacamaraappplantas.TutorialOverlay
-import com.example.pruebacamaraappplantas.databinding.ActivityTutorialBinding
-import com.example.pruebacamaraappplantas.fragments.TutorialFragment
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class TutorialActivity : AppCompatActivity() {
+    private var overlayView: View? = null
+    private var stepIndex = 0
+    private var bottomNavigationView: BottomNavigationView? = null
+    private lateinit var bottomAppBar: BottomAppBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+        // Desactivar el modo nocturno
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
-        val tutorial = TutorialOverlay(this)
-        tutorial.startTutorial()
+
+        // Iniciar el tutorial
+        startTutorial()
+    }
+
+    private fun startTutorial() {
+        showOverlay()
+        showStep()
+    }
+
+    private fun showOverlay() {
+        val rootView = window.decorView.rootView as ViewGroup
+        overlayView = LayoutInflater.from(this).inflate(R.layout.overlay_tutorial, rootView, false)
+        rootView.addView(overlayView)
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation)
+        bottomAppBar = findViewById(R.id.bottomAppBar)
+
+        bottomAppBar.menu.setGroupEnabled(0, false)
+
+        findViewById<View>(R.id.btnSaltarTutorial)
+            .setOnClickListener { v: View? -> endTutorial() }
+        findViewById<View>(R.id.halfRightView)
+            .setOnClickListener { v: View? -> nextStep() }
+        findViewById<View>(R.id.halfLeftView)
+            .setOnClickListener { v: View? -> previousStep() }
+    }
+
+    private fun showStep() {
+        when (stepIndex) {
+            0 -> {
+                findViewById<View>(R.id.view1).visibility = View.VISIBLE
+                findViewById<View>(R.id.imagentuto1).visibility = View.VISIBLE
+                findViewById<View>(R.id.imagentuto2).visibility = View.INVISIBLE
+                findViewById<View>(R.id.imagentuto3).visibility = View.INVISIBLE
+                findViewById<View>(R.id.ranking).setBackgroundColor(Color.parseColor("#99000000"))
+            }
+
+            1 -> {
+                findViewById<View>(R.id.view1).visibility = View.INVISIBLE
+                findViewById<View>(R.id.imagentuto1).visibility = View.INVISIBLE
+                findViewById<View>(R.id.imagentuto3).visibility = View.VISIBLE
+                findViewById<View>(R.id.imagentuto2).visibility = View.INVISIBLE
+                findViewById<View>(R.id.ranking).setBackgroundColor(Color.WHITE)
+                findViewById<View>(R.id.calendarView).setBackgroundColor(Color.parseColor("#70000000"))
+            }
+
+            2 -> {
+                findViewById<View>(R.id.imagentuto1).visibility = View.INVISIBLE
+                findViewById<View>(R.id.imagentuto3).visibility = View.INVISIBLE
+                findViewById<View>(R.id.imagentuto2).visibility = View.VISIBLE
+                findViewById<View>(R.id.imagentuto4).visibility = View.INVISIBLE
+                findViewById<View>(R.id.ranking).setBackgroundColor(Color.parseColor("#99000000"))
+                findViewById<View>(R.id.calendarView).setBackgroundColor(Color.WHITE)
+            }
+
+            3 -> {
+                findViewById<View>(R.id.imagentuto2).visibility = View.INVISIBLE
+                findViewById<View>(R.id.calendarView).setBackgroundColor(Color.parseColor("#70000000"))
+                findViewById<View>(R.id.imagentuto4).visibility = View.VISIBLE
+                findViewById<View>(R.id.imagentuto5).visibility = View.INVISIBLE
+                updateBottomBarSelection(R.id.nav_home)
+            }
+
+            4 -> {
+                findViewById<View>(R.id.imagentuto4).visibility = View.INVISIBLE
+                findViewById<View>(R.id.imagentuto5).visibility = View.VISIBLE
+                findViewById<View>(R.id.imagentuto6).visibility = View.INVISIBLE
+                updateBottomBarSelection(R.id.nav_rankings)
+            }
+
+            5 -> {
+                findViewById<View>(R.id.imagentuto5).visibility = View.INVISIBLE
+                findViewById<View>(R.id.imagentuto6).visibility = View.VISIBLE
+                findViewById<View>(R.id.imagentuto7).visibility = View.INVISIBLE
+                updateBottomBarSelection(R.id.nav_library)
+            }
+
+            6 -> {
+                findViewById<View>(R.id.imagentuto6).visibility = View.INVISIBLE
+                findViewById<View>(R.id.imagentuto7).visibility = View.VISIBLE
+                updateBottomBarSelection(R.id.nav_settings)
+            }
+        }
+    }
+
+    private fun updateBottomBarSelection(itemId: Int) {
+        if (bottomNavigationView != null) {
+            bottomNavigationView!!.post {
+                val menuItem = bottomNavigationView!!.menu.findItem(itemId)
+                menuItem?.setChecked(true)
+                    ?: Log.d("TutorialActivity", "Menu item not found: $itemId")
+            }
+        }
+    }
+
+    private fun nextStep() {
+        stepIndex++
+        if (stepIndex > 6) {
+            endTutorial()
+        } else {
+            showStep()
+        }
+    }
+
+    private fun previousStep() {
+        stepIndex--
+        if (stepIndex >= 0) {
+            showStep()
+        }
+    }
+
+    private fun endTutorial() {
+        val rootView = window.decorView.rootView as ViewGroup
+        rootView.removeView(overlayView)
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
--- app/src/main/java/com/example/pruebacamaraappplantas/TutorialOverlay.kt
+++ app/src/main/java/com/example/pruebacamaraappplantas/TutorialOverlay.kt
@@ -5,16 +5,19 @@ import android.content.Intent
 import android.graphics.Color
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.TextView
 import com.example.pruebacamaraappplantas.Activities.MainActivity
+import com.google.android.material.bottomappbar.BottomAppBar
 
 class TutorialOverlay(private val activity: Activity) {
     private var overlayView: View? = null
     private var stepIndex = 0
+    private lateinit var bottomAppBar: BottomAppBar
 
     fun startTutorial() {
         showOverlay()
@@ -26,6 +29,9 @@ class TutorialOverlay(private val activity: Activity) {
         overlayView = LayoutInflater.from(activity).inflate(R.layout.overlay_tutorial, rootView, false)
         rootView.addView(overlayView)
 
+        overlayView?.let { view ->
+            bottomAppBar = view.findViewById(R.id.bottomAppBar)
+        }
         overlayView?.findViewById<Button>(R.id.btnSaltarTutorial)?.setOnClickListener {
             endTutorial()
         }
@@ -69,10 +75,10 @@ class TutorialOverlay(private val activity: Activity) {
                 val imagentuto1 = activity.findViewById<View>(R.id.imagentuto1)
                 imagentuto1?.visibility = View.INVISIBLE
 
-                val imagentuto2 = activity.findViewById<View>(R.id.imagentuto2)
+                val imagentuto2 = activity.findViewById<View>(R.id.imagentuto3)
                 imagentuto2?.visibility = View.VISIBLE
 
-                val imagentuto3 = activity.findViewById<View>(R.id.imagentuto3)
+                val imagentuto3 = activity.findViewById<View>(R.id.imagentuto2)
                 imagentuto3?.visibility = View.INVISIBLE
 
 
@@ -91,11 +97,15 @@ class TutorialOverlay(private val activity: Activity) {
                 val imagentuto1 = activity.findViewById<View>(R.id.imagentuto1)
                 imagentuto1?.visibility = View.INVISIBLE
 
-                val imagentuto2 = activity.findViewById<View>(R.id.imagentuto2)
+                val imagentuto2 = activity.findViewById<View>(R.id.imagentuto3)
                 imagentuto2?.visibility = View.INVISIBLE
 
-                val imagentuto3 = activity.findViewById<View>(R.id.imagentuto3)
+                val imagentuto3 = activity.findViewById<View>(R.id.imagentuto2)
                 imagentuto3?.visibility = View.VISIBLE
+
+                val imagentuto4 = activity.findViewById<View>(R.id.imagentuto4)
+                imagentuto4?.visibility = View.INVISIBLE
+
                 // Paso 3: Cambiar el fondo del ranking a rojo oscuro y el calendario a un color más oscuro
                 val ranking = activity.findViewById<View>(R.id.ranking)
                 ranking?.let {
@@ -107,12 +117,86 @@ class TutorialOverlay(private val activity: Activity) {
                     it.setBackgroundColor(Color.WHITE) // Cambiar fondo del ranking a blanco
                 }
             }
+
+            3 -> {
+
+                val imagentuto3 = activity.findViewById<View>(R.id.imagentuto2)
+                imagentuto3?.visibility = View.INVISIBLE
+
+                val calendar = activity.findViewById<View>(R.id.calendarView)
+                calendar?.let {
+                    it.setBackgroundColor(Color.parseColor("#70000000")) // Fondo oscuro para el ranking
+                }
+
+                val imagentuto4 = activity.findViewById<View>(R.id.imagentuto4)
+                imagentuto4?.visibility = View.VISIBLE
+
+                val imagentuto5 = activity.findViewById<View>(R.id.imagentuto5)
+                imagentuto5?.visibility = View.INVISIBLE
+
+                bottomAppBar?.post {
+                    val menuItem = bottomAppBar.menu.findItem(R.id.nav_library)
+                    if (menuItem != null) {
+                        Log.d("TutorialOverlay", "Menu item found: $menuItem")
+                        menuItem.isChecked = true
+                    } else {
+                        Log.d("TutorialOverlay", "Menu item not found")
+                    }
+                }
+
+            }
+
+            4 -> {
+
+                val imagentuto4 = activity.findViewById<View>(R.id.imagentuto4)
+                imagentuto4?.visibility = View.INVISIBLE
+
+                val imagentuto5 = activity.findViewById<View>(R.id.imagentuto5)
+                imagentuto5?.visibility = View.VISIBLE
+
+                val imagentuto6 = activity.findViewById<View>(R.id.imagentuto6)
+                imagentuto6?.visibility = View.INVISIBLE
+
+                bottomAppBar?.post {
+                    val menuItem = bottomAppBar.menu.findItem(R.id.nav_rankings)
+                    if (menuItem != null) {
+                        Log.d("TutorialOverlay", "Menu item found: $menuItem")
+                        menuItem.isChecked = true
+                    } else {
+                        Log.d("TutorialOverlay", "Menu item not found")
+                    }
+                }
+
+
+            }
+
+            5 -> {
+
+                val imagentuto5 = activity.findViewById<View>(R.id.imagentuto5)
+                imagentuto5?.visibility = View.INVISIBLE
+
+                val imagentuto6 = activity.findViewById<View>(R.id.imagentuto6)
+                imagentuto6?.visibility = View.VISIBLE
+
+                val imagentuto7 = activity.findViewById<View>(R.id.imagentuto7)
+                imagentuto7?.visibility = View.INVISIBLE
+            }
+
+            6 -> {
+
+                val imagentuto6 = activity.findViewById<View>(R.id.imagentuto6)
+                imagentuto6?.visibility = View.INVISIBLE
+
+                val imagentuto7 = activity.findViewById<View>(R.id.imagentuto7)
+                imagentuto7?.visibility = View.VISIBLE
+
+            }
         }
     }
 
     private fun nextStep() {
         stepIndex++
-        if (stepIndex > 2) {
+        if (stepIndex > 6) {
             endTutorial()
         } else {
             showStep()
